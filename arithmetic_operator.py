4338251a4dba2b2d9992689b209bb86b42db3a6f
Majid Bahader <majid.bahader@dci-student.org>
2023-10-24 14:33:05 +0200

    arithmetic operator exercise

--- arithmetic_operator.py
+++ arithmetic_operator.py
@@ -4,3 +4,17 @@ var_b = 1500 + 3000.00 # float
 
 print("var_a:", var_a)
 print("var_b:", var_b)
+
+# var_a is an integer, when we add them the result is an integer 
+# variable_b is a float because when we add them the result is a float
+
+# var_c is an integer as the result of 1000/500 is equal to 200 which is an integer
+
+#var_d = 1200 + "200"
+#print(var_d)
+
+# var_d is the result of the expression
+# when the integer is add on to a string, it does not concatenate but results in an error, the operator + does not concatenate a string with an integer 
+
+var_e = "3004" + "996"
+print(var_e)
